ce6f1980131de4ca2a2111b736f0b7744fe870ba
Kalini Jithma <109453437+kalinijithma@users.noreply.github.com>
2024-02-24 23:21:06 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,24 +1,16 @@
 const imgs = document.querySelectorAll('.img-select a');
+const imgBtns = [...imgs];
+let imgId = 1;
 
-const imgBtns =[... imgs];
-
-let imgid=1;
-
-imgBtns.forEach((imgItem)=>{
-
-ingItem.addEventListener('click', (event) =>{
-
-event.preventDefault(); 
-imgid = imgitem.dataset.id;
-
-slideImage();
-});
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
 });
 
-function slideImage(){
-
-const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
-
-document.querySelector('.img-showcase').style.transform = "translateX($((imgId1) displayWidth}px)";
+function slideImage() {
+    const displayWidth = document.querySelector('.img-showcase').clientWidth;
+    document.querySelector('.img-showcase').style.transform = `translateX(${-((imgId - 1) * displayWidth)}px)`;
 }
-
