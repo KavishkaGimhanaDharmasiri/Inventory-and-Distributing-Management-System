d4a2a1b757e0df15d7e4eaaaa1d3678676a208a2
Gayantha Maduwantha <prolinkpc2@gmail.com>
2024-03-01 09:06:49 +0530

    revoke

--- modmun.php
+++
@@ -1,5 +0,0 @@
- <?php 
- $telephone=714046282;
-$modifiedNumber = '94' . substr($telephone, 0);
-echo $modifiedNumber ;
-  ?>
--- vsd.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo'<script>alert("hello");</script>';
-   ob_end_clean();
-  require_once('vbs.php');
-  ob_end_clean();
-?>
